b676dfa62966d5542dc110f30677f33dc916d847
svro <ichistrawberry2012@gmail.com>
2023-09-07 10:09:23 +0300

    model lesson

--- education/models.py
+++ education/models.py
@@ -19,3 +19,21 @@ class Course(models.Model):
         verbose_name = 'курс'
         verbose_name_plural = 'курсы'
         ordering = ('title',)
+
+
+# Lesson
+
+class Lesson(models.Model):
+    '''Урок'''
+    title = models.CharField(max_length=200, verbose_name='Название')
+    preview = models.ImageField(**NULLABLE, verbose_name='Превью')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    link_video = models.CharField(**NULLABLE, max_length=200, verbose_name='Ссылка на видео')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
+        ordering = ('title',)
