45932d2c354602ef209314de613748baffad7afc
svro <ichistrawberry2012@gmail.com>
2023-09-07 11:06:19 +0300

    CourseViewSet

--- education/views.py
+++ education/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from education.models import Course
+from education.serializers import CourseSerializer
 
-# Create your views here.
+
+'''COURSE'''
+class CourseViewSet(viewsets.ModelViewSet):
+    '''вьюсет Курса'''
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+'''LESSON'''
