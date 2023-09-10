c8af656452693ffc00530bb9231eb9abbec2a387
svro <ichistrawberry2012@gmail.com>
2023-09-10 12:51:04 +0300

    payment course serializers

--- education/serializers.py
+++ education/serializers.py
@@ -1,14 +1,28 @@
 from rest_framework import serializers
-from education.models import Course, Lesson
-
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
+from education.models import Course, Lesson, Payment
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    '''Для сериализатора модели Course реализуем поле вывода уроков - lesson'''
+    lesson_count = serializers.SerializerMethodField()
+    lesson = LessonSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['title', 'preview', 'description', 'lesson_count', 'lesson']
+
+    @staticmethod
+    def get_lesson_count(instanse):
+        return instanse.lesson.count()
